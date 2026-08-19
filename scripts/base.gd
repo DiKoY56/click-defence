ab35c9b771393f8d74c5b8dce432bf67d43c0379
DiKoY56 <proelectr@proton.me>
2026-08-20 01:11:52 +0500

    Add sound of taking damage from the base

--- scripts/base.gd
+++ scripts/base.gd
@@ -6,6 +6,8 @@ signal destroyed
 
 @export var max_health: int = 20
 
+const DamageBaseSound: AudioStream = preload("res://assets/audio/damage_base.wav")
+
 var health: int
 
 func _ready() -> void:
@@ -15,6 +17,7 @@ func take_damage(amount: int) -> void:
 	if health <= 0:
 		return
 	health = max(health - amount , 0)
+	play_damage_sound()
 	print("HP базы: ", health)
 	health_changed.emit(health)
 	if health <= 0:
@@ -22,6 +25,14 @@ func take_damage(amount: int) -> void:
 
 func die() -> void:
 	$Sprite2D.modulate=Color.RED
-	print("База уничтожена")
 	destroyed.emit()
 	
+func play_damage_sound() -> void:
+	var player := AudioStreamPlayer2D.new()
+	player.stream = DamageBaseSound
+	player.position = global_position
+	player.pitch_scale = randf_range(0.90, 1.05)
+	player.volume_db = -8
+	get_tree().current_scene.add_child(player)
+	player.finished.connect(player.queue_free)
+	player.play()
