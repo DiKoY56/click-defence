54bb451914ecbd5d90e7ce7ba46a1ffa615d62e9
DiKoY56 <proelectr@proton.me>
2026-09-21 00:04:40 +0500

    add Settings menu

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -3,14 +3,10 @@ extends Node
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed \
 		and (event.keycode == KEY_F11 or (event.alt_pressed and event.keycode == KEY_ENTER)):
-		toggle_fullscreen()
+		SaveManager.set_fullscreen(not SaveManager.fullscreen)
 
 func toggle_fullscreen() -> void:
 	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-
-func _ready() -> void:
-	if not OS.has_feature("editor"):
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
